7455bf0687f940b53f14c4fe33b0292eb35fc8de
Bryce Kan <brycekan123@gmail.com>
2024-10-26 14:52:43 -0700

    retry

--- ClusteredonSampleData.py
+++ ClusteredonSampleData.py
@@ -12,32 +12,82 @@ print("Original Data:")
 unmatched_group = []
 result_list = []
 ##GROUP2
-group_pref_2 = data.loc[(data["Group Size Preference"] == 2)].copy()
+# group_pref_2 = data.loc[(data["Group Size Preference"] == 2)].copy()
 
-result_list = []
-match_counter = 0
-group_pref_2 = MatchingFunctions.exactMatch(group_pref_2,result_list)
+# group_pref_2 = MatchingFunctions.exactMatch(group_pref_2,result_list)
+# templist = []
+# groupsize = 2
+# while len(group_pref_2) > 0:
+#     first_row = group_pref_2.iloc[0]
+#     max_count = -1
+#     index_to_delete = -1
+#     selected_day = ""  
+#     #added
+#     if first_row["Email"] not in templist:
+#          templist.append(first_row["Email"])
+#     # Step 3: Loop through all other rows
+#     for i in range(1, len(group_pref_2)):
+#         current_row = group_pref_2.iloc[i]
+#         print("current row",current_row)
+
+#         count = 0
+#         available_days_first = set(day.strip() for day in first_row['Availability'].split(";"))
+#         available_days_current = set(day.strip() for day in current_row['Availability'].split(";"))
+#         common_days = available_days_first.intersection(available_days_current)
+#         print("first",available_days_first,"current",available_days_current,"common",common_days)
+#         if common_days:
+#             print("inside")
+#             selected_day = random.choice(list(common_days))
+#             matched_criteria = []
+#             if first_row['Preferred Language'] == current_row['Preferred Language']:
+#                 matched_criteria.append(first_row['Preferred Language'])
+#                 count += 1
+#             if first_row['Completed Class'] == current_row['Completed Class']:
+#                 matched_criteria.append(first_row['Completed Class'])
+#                 count += 1
+#             if count > max_count:
+#                 matched_criteria.append(selected_day)
+#                 max_count = count
+#                 index_to_delete = i
+#                 definiteMatch = []
+#                 definiteMatch.append(matched_criteria)
+
+#     print("max_count",max_count,"index matched",index_to_delete)
+#     group_pref_2,templist= MatchingFunctions.checkMatchConditions(
+#         group_pref_2,index_to_delete,templist,groupsize)
+#     group_pref_2,result_list,unmatched_group,templist,selected_day = (
+#         MatchingFunctions.finishGroup(
+#         group_pref_2,index_to_delete,result_list,templist,groupsize,unmatched_group,first_row,selected_day
+#         )
+#     )
+# print(result_list)
+# print("#ofmatches",len(result_list))
+
+# print("UNMATCHED",unmatched_group)
+
+groupsize=3
+group_pref_3  = data.loc[(data["Group Size Preference"] == 3)].copy()
+group_pref_3.reset_index(drop=True, inplace=True)
+print(group_pref_3)
 templist = []
-while len(group_pref_2) > 0:
-    first_row = group_pref_2.iloc[0]
+selected_day = ""  
+while len(group_pref_3) > 0:
+    first_row = group_pref_3.iloc[0]
+    if first_row["Email"] not in templist:
+        templist.append(first_row["Email"])
     max_count = -1
     index_to_delete = -1
-    selected_day = ""  
-    #added
-    if first_row["Email"] not in templist:
-         templist.append(first_row["Email"])
-    # Step 3: Loop through all other rows
-    for i in range(1, len(group_pref_2)):
-        current_row = group_pref_2.iloc[i]
-        print("current row",current_row)
-
+    for i in range(1, len(group_pref_3)):
+        current_row = group_pref_3.iloc[i]
         count = 0
-        available_days_first = set(day.strip() for day in first_row['Availability'].split(";"))
+        if selected_day != "":
+            available_days_first = {selected_day}
+        else:
+            available_days_first = set(day.strip() for day in first_row['Availability'].split(";"))
         available_days_current = set(day.strip() for day in current_row['Availability'].split(";"))
+
         common_days = available_days_first.intersection(available_days_current)
-        print("first",available_days_first,"current",available_days_current,"common",common_days)
         if common_days:
-            print("inside")
             selected_day = random.choice(list(common_days))
             if first_row['Preferred Language'] == current_row['Preferred Language']:
                 count += 1
@@ -46,80 +96,19 @@ while len(group_pref_2) > 0:
             if count > max_count:
                 max_count = count
                 index_to_delete = i
-
-    print("max_count",max_count,"index matched",index_to_delete)
-    group_pref_2,templist= MatchingFunctions.checkMatchConditions(group_pref_2,index_to_delete,templist,2)
-
-        # result_list.append([first_row['Email'],group_pref_2.iloc[index_to_delete]['Email'],selected_day])
-        # print("MATCH",first_row['Email'],"matched with",group_pref_2.iloc[index_to_delete]['Email'])
-        # group_pref_2 = group_pref_2.drop([0, index_to_delete]).reset_index(drop=True)
-    if index_to_delete != -1 and len(templist)== 2:
-        result_list.append([templist,selected_day])
-        group_pref_2 = group_pref_2.drop([0]).reset_index(drop=True)
-        selected_day = ""  
-        templist = []
-    else:
-        group_pref_2 = group_pref_2.drop([0]).reset_index(drop=True)
-        unmatched_group.append(first_row['Email'])
-print(result_list)
-print("#ofmatches",len(result_list))
-
-print("UNMATCHED",unmatched_group)
-
-#GROUP3 MATCH DOESN'T WORK
-# group_pref_3  = data.loc[(data["Group Size Preference"] == 3)].copy()
-# group_pref_3.reset_index(drop=True, inplace=True)
-# print(group_pref_3)
-# templist = []
-# selected_day = ""  
-# while len(group_pref_3) > 0:
-#     first_row = group_pref_3.iloc[0]
-#     if first_row["Email"] not in templist:
-#         templist.append(first_row["Email"])
-#     max_count = -1
-#     index_to_delete = -1
-#     for i in range(1, len(group_pref_3)):
-#         current_row = group_pref_3.iloc[i]
-#         count = 0
-#         if selected_day != "":
-#             available_days_first = {selected_day}
-#         else:
-#             available_days_first = set(day.strip() for day in first_row['Availability'].split(";"))
-#         available_days_current = set(day.strip() for day in current_row['Availability'].split(";"))
-
-#         common_days = available_days_first.intersection(available_days_current)
-#         if common_days:
-#             selected_day = random.choice(list(common_days))
-#             if first_row['Preferred Language'] == current_row['Preferred Language']:
-#                 count += 1
-#             if first_row['Completed Class'] == current_row['Completed Class']:
-#                 count += 1
-#             if count > max_count:
-#                 max_count = count
-#                 index_to_delete = i
                 
-#     # HAVE FOUND A MATCH. deleting current row
-#     if index_to_delete != -1 and len(templist)< 3:
-#         # print("inside group")
-#         print(group_pref_3)
-#         print(group_pref_3.iloc[index_to_delete]['Email'],index_to_delete)
-#         templist.append(group_pref_3.iloc[index_to_delete]['Email'])
-#         group_pref_3 = group_pref_3.drop([index_to_delete]).reset_index(drop=True)
+    # HAVE FOUND A MATCH. deleting current row
+    group_pref_3,templist= MatchingFunctions.checkMatchConditions(
+        group_pref_3,index_to_delete,templist,groupsize)
+    group_pref_3,result_list,unmatched_group,templist,selected_day = (
+        MatchingFunctions.finishGroup(
+        group_pref_3,index_to_delete,result_list,templist,groupsize,unmatched_group,first_row,selected_day
+        )
+    )
 
-#     ## 2 MATCHES FOUND. now deleting first row, move on to next "first iteration"
-#     if index_to_delete != -1 and len(templist)== 3:
-#         result_list.append([templist,selected_day])
-#         group_pref_3 = group_pref_3.drop([0]).reset_index(drop=True)
-#         selected_day = ""  
-#         templist = []
-        
-#     if index_to_delete == -1:
-#         unmatched_group.append(first_row['Email'])
-#         group_pref_3 = group_pref_3.drop([0]).reset_index(drop=True)
+print(result_list)
 
-# print(result_list)
-
-# print(len(result_list))
+print(len(result_list))
 
         
     
