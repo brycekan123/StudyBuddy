1f0c86ea15bb4abc784cb6947197e74d659806d9
Bryce Kan <brycekan123@gmail.com>
2024-10-26 20:36:47 -0700

    unmatched groups done

--- ClusteredonSampleData.py
+++ ClusteredonSampleData.py
@@ -33,7 +33,9 @@ while len(group_pref_2) > 0:
         count = 0
         available_days_first = set(day.strip() for day in first_row['Availability'].split(";"))
         available_days_current = set(day.strip() for day in current_row['Availability'].split(";"))
+
         common_days = available_days_first.intersection(available_days_current)
+        #common_days = MatchingFunctions.find_common_days(first_row['Availability'], current_row['Availability'])
         if common_days:
             selected_day = random.choice(list(common_days))
             matched_criteria = []
@@ -47,7 +49,6 @@ while len(group_pref_2) > 0:
                 matched_criteria.append(selected_day)
                 max_count = count
                 index_to_delete = i
-                definiteMatch = []
                 definiteMatch = matched_criteria
     
     print(group_pref_2)
@@ -80,6 +81,8 @@ print("UNMATCHED",unmatched_group)
 #     for i in range(1, len(group_pref_3)):
 #         current_row = group_pref_3.iloc[i]
 #         count = 0
+#         match_criteria = []
+
 #         if selected_day != "":
 #             available_days_first = {selected_day}
 #         else:
@@ -90,25 +93,33 @@ print("UNMATCHED",unmatched_group)
 #         if common_days:
 #             selected_day = random.choice(list(common_days))
 #             if first_row['Preferred Language'] == current_row['Preferred Language']:
+#                 match_criteria.append(first_row['Preferred Language'])
 #                 count += 1
 #             if first_row['Completed Class'] == current_row['Completed Class']:
+#                 match_criteria.append(first_row['Completed Class'])
 #                 count += 1
 #             if count > max_count:
+#                 match_criteria.append(selected_day)
 #                 max_count = count
 #                 index_to_delete = i
+#                 #definiteMatch will only update if curRow == max row. Will be passed to resultList
+#                 definiteMatch = match_criteria
+
                 
 #     # HAVE FOUND A MATCH. deleting current row
 #     group_pref_3,templist= MatchingFunctions.checkMatchConditions(
 #         group_pref_3,index_to_delete,templist,groupsize)
 #     group_pref_3,result_list,unmatched_group,templist,selected_day = (
 #         MatchingFunctions.finishGroup(
-#         group_pref_3,index_to_delete,result_list,templist,groupsize,unmatched_group,first_row,selected_day
+#         group_pref_3,index_to_delete,result_list,templist,groupsize,unmatched_group,first_row,selected_day,definiteMatch
 #         )
 #     )
 
 # print(result_list)
 
 # print(len(result_list))
+
+# print(unmatched_group)
 
         
     
