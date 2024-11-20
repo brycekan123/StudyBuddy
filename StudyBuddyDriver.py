726386b83b586a08fe6707f440c64323e26263ac
Bryce Kan <brycekan123@gmail.com>
2024-11-20 13:49:53 -0800

    modifiedtoairtableinput

--- StudyBuddyDriver.py
+++ StudyBuddyDriver.py
@@ -4,24 +4,24 @@ from ClusteredonSampleData import GroupMatcher
 import pandas as pd
 def run_matching():
     # Set the path to your CSV file
-    csv_file_path = '/Users/brycekan/Desktop/StudyBuddyNov17/StudyBuddy/testSampleData.csv'  # Update with your actual path.
+    csv_file_path = '/Users/brycekan/Desktop/StudyBuddyNov17/StudyBuddy/test.csv'
 
     # Initialize the GroupMatcher with the CSV file path
     matcher = GroupMatcher(csv_file_path)
 
     # Call the match_groups method to perform the matching
-    group_sizes = [2,3]
+    group_sizes = [3]
     for group_size in group_sizes:
         matcher.process_group(group_size)
     getInfo(matcher.getResultList(), matcher.getUnmatchedList())
-    result_df = pd.DataFrame(matcher.getResultList())
-    unmatched_df = pd.DataFrame(matcher.getUnmatchedList())
+    # result_df = pd.DataFrame(matcher.getResultList())
+    # unmatched_df = pd.DataFrame(matcher.getUnmatchedList())
 
     # Save to CSV
-    result_df.to_csv('matched_groups.csv', index=False)
-    unmatched_df.to_csv('unmatched_groups.csv', index=False)
+    # result_df.to_csv('matched_groups.csv', index=False)
+    # unmatched_df.to_csv('unmatched_groups.csv', index=False)
 
-    print("Results saved to 'matched_groups.csv' and 'unmatched_groups.csv'")
+    # print("Results saved to 'matched_groups.csv' and 'unmatched_groups.csv'")
 
 print("Group matching process completed!")
 
